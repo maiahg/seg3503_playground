9f79975fe52c65ecc3af650400a554d72426de12
maiahg <122498165+maiahg@users.noreply.github.com>
2026-06-25 18:39:55 -0400

    failing test 2

--- lab04/tic_java/TicTest.java
+++ lab04/tic_java/TicTest.java
@@ -12,4 +12,13 @@ class TicTest {
 
 		assertEquals(board, board2);
 	}
+
+	@Test
+	void playingFirstMoveShouldPutXOnBoard() {
+		Tic board = new Tic(3, 3);
+
+		board.play(0, 0);
+
+		assertEquals("X", board.board[0][0]);
+	}
 }
