082f6156cc678e3741efa7cf1322ec5b4d4236ef
maiahg <122498165+maiahg@users.noreply.github.com>
2026-06-25 18:20:00 -0400

    failing test 1

--- lab04/tic_java/TicTest.java
+++ lab04/tic_java/TicTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 	@Test
-	void test() {
+	void twoNewBoardsWithSameSizeShouldBeEqual() {
 		Tic board = new Tic(3, 3);
-		Tic board2 = new Tic(3,3);
-		assertEquals(board, board2);
-		//testing ability to declare a board
-	}
+		Tic board2 = new Tic(3, 3);
 	
+		assertEquals(board, board2);
+	}
 }
