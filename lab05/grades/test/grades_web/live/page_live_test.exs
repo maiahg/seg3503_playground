d284520274ada1e47205138ce0b579c4775bd620
maiahg <122498165+maiahg@users.noreply.github.com>
2026-07-01 17:36:03 -0400

    lab05: add grade stubbed code

--- lab05/grades/test/grades_web/live/page_live_test.exs
+++ lab05/grades/test/grades_web/live/page_live_test.exs
@@ -8,4 +8,28 @@ defmodule GradesWeb.PageLiveTest do
     assert disconnected_html =~ "Grades"
     assert render(page_live) =~ "Grades"
   end
+
+  test "submitting an empty grades form does not crash", %{conn: conn} do
+    {:ok, page_live, _html} = live(conn, "/")
+
+    html =
+      page_live
+      |> form("form", %{
+        "h1" => "",
+        "h2" => "",
+        "h3" => "",
+        "h4" => "",
+        "midterm" => "",
+        "final" => "",
+        "l1" => "",
+        "l2" => "",
+        "l3" => "",
+        "l4" => "",
+        "l5" => "",
+        "l6" => ""
+      })
+      |> render_submit()
+
+    assert html =~ "Final Grade"
+  end
 end
